ad441a5a5e4f198fe9048359c1fad17b2f7f28d9
Vadim Khaitov <polydora@rambler.ru>
2021-04-09 14:38:46 +0300

    много всего

--- Mussel_Cancer/Rcalc_for_cancer/processing.R
+++ Mussel_Cancer/Rcalc_for_cancer/processing.R
@@ -1,11 +1,19 @@
 library(readxl)
 library(dplyr)
-
+library(ggplot2)
 
 point <- read_excel("Data/Point_presence.xlsx", na = "NA") 
+
+
+point <- point %>%  mutate(Npoint = pmax(Left,Right))
 
 point %>% filter(Seria == "J") %>% filter(Cancer == "No") %>%  mutate(Npoint = round((Left + Right)/2)) %>% filter(Npoint > quantile(Npoint)[4]) %>% select(Number) %>% pull() 
 
 
 
-point %>% filter(Seria == "J") %>% filter(Cancer == "No") %>%  mutate(Npoint = round((Left + Right)/2)) %>% filter(Npoint < quantile(Npoint)[4]) %>% select(Number)  %>% pull() %>% sample(., 4)
+point %>% filter(!is.na(Seria)) %>% 
+  ggplot(., aes(x = Seria, y = Npoint, fill = Cancer)) + geom_boxplot(varwidth = T) + scale_fill_manual(values = c("green", "red")) + labs(x = "Точка сбора", y = "Количество точечек на раковине")
+
+
+point %>% filter(Seria == "J") %>% 
+  ggplot(., aes(x = Cancer, y = Npoint)) + geom_boxplot()
