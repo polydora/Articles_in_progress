b70fdc3e929ac6693d073ac175477fd300edbe33
Vadim Khaitov <polydora@rambler.ru>
2021-10-23 12:51:47 +0300

    много всего с гибридами

--- Mytilus hybrids/R_calc_Mytilus_hybrids/hybrids_processing.R
+++ Mytilus hybrids/R_calc_Mytilus_hybrids/hybrids_processing.R
@@ -1,19 +1,103 @@
 library(readxl)
 library(ggplot2)
 library(dplyr)
+library(mgcv)
+
+
 
 hybr <- read_excel("Data/hybrids_BS.xlsx")
 murm_shape <- read.csv("Data/Murman.csv")
 
+str(hybr)
+
+
+hybr$region <- factor(hybr$region)
+
+hybr$Sea <- with(hybr, ifelse(region == "WS", "WS", "BS"))
+
 
 # Доля гибридов в сборах
 
-hybr_pop <- hybr %>% group_by(pop) %>% summarise(lat = mean(N), lon = mean(E), Year = mean(year), Ptros = mean(str), P_hybr = mean(str > 0.2 & str < 0.8), md = 1 - (sd(str))^2/(Ptros*(1-Ptros)) )
+hybr_pop <- hybr %>% group_by(ID) %>% summarise(lat = mean(N), lon = mean(E), Year = mean(year), Region = unique(region), Ptros = mean(str), P_hybr = mean(str > 0.05 & str < 0.95), md = 1 - (sd(str))^2/(Ptros*(1-Ptros)), md_1 = 1-md, Salinity = unique(salinity) ) 
+# %>% filter(Ptros<0.9 & Ptros>0.1)
 
 
-ggplot(hybr_pop, aes(x = P_hybr, y = md)) + geom_point()
+hybr_pop$Region <- factor(hybr_pop$Region, levels = c("West_coast",  "Kola",  "Tyuva", "East_coast" ))
 
-hybr_pop <- hybr_pop %>% arrange(desc(P_hybr))
+ggplot(hybr_pop, aes(x = Ptros, y = md_1)) +  geom_point() + geom_smooth()
+
+ggplot(hybr_pop, aes(x = Region, y = md_1))  + geom_hline(yintercept = median(hybr_pop$md)) + geom_boxplot()
+
+
+
+ggplot(hybr_pop, aes(x = P_hybr, y = md_1)) +  geom_point()
+
+
+
+Mod <-  gam(md_1 ~ s(Ptros, k = 5)  + Salinity , data = hybr_pop, family = "betar" ) 
+summary(Mod)
+
+
+
+My_data <- expand.grid(Ptros = seq(0, 1, 0.01),  Salinity = c("normal", "brackish"))
+
+
+Predicted <- predict(Mod, newdata = My_data, type = "response", se.fit = T) 
+
+My_data$fit <- Predicted$fit
+My_data$se <- Predicted$se.fit
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop %>%  filter(Region == "East_coast"), aes(y = md_1), size = 3)  
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop %>%  filter(Region == "Kola"), aes(y = md_1), size = 3)  
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop %>%  filter(Region == "Tyuva"), aes(y = md_1), size = 3)  
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop %>%  filter(Region == "West_coast"), aes(y = md_1), size = 3)  
+
+
+
+Mod2 <-  gam(P_hybr ~ s(Ptros, k = 5)  + Salinity , data = hybr_pop, family = "betar" ) 
+summary(Mod2)
+
+
+plot(Mod2)
+
+
+Predicted <- predict(Mod2, newdata = My_data, type = "response", se.fit = T) 
+
+My_data$fit <- Predicted$fit
+My_data$se <- Predicted$se.fit
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop, aes(y = P_hybr), size = 3)  + geom_point(data = hybr_pop %>% filter(Region %in% c("Tyuva")), aes(y = P_hybr), color = "black")
+
+
+
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop %>%  filter(Region == "Kola"), aes(y = md_1), size = 3)  
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop %>%  filter(Region == "Tyuva"), aes(y = md_1), size = 3)  
+
+ggplot(My_data, aes(x = Ptros, y = fit, color = Salinity)) + geom_line() + geom_point(data = hybr_pop %>%  filter(Region == "West_coast"), aes(y = md_1), size = 3)  
+
+
+
+
+
+Mod_resid <- residuals(Mod)
+
+ggplot(hybr_pop, aes(x = factor(Year), group = Year, y = Mod_resid)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 90))   
+
+
+  
+qplot(x = hybr_pop$P_hybr, y = Mod_resid)
+
+
+# + geom_ribbon(aes(ymin = fit - 1.96*se, ymax = fit+1.96*se), alpha = 0.1)  
+
+
+
+
 
 
 # Рисуем карту
@@ -66,12 +150,12 @@ Tuva_map <-
 
 
 Murm_region_map + 
-  geom_point(data = hybr_pop, aes(x = lon, y = lat, group = 1, fill = P_hybr, size = P_hybr), shape = 21) +
+  geom_point(data = hybr_pop, aes(x = lon, y = lat, group = 1, fill = Mod_gam_resid, size = Mod_gam_resid), shape = 21) +
   scale_fill_gradient(low = "yellow", high = "red")
 
 
 Kola_bay_map + 
-  geom_point(data = hybr_pop, aes(x = lon, y = lat, group = 1, fill = P_hybr, size = P_hybr), shape = 21) +
+  geom_point(data = hybr_pop, aes(x = lon, y = lat, group = 1, fill = md_1, size = md_1), shape = 21) +
   scale_fill_gradient(low = "yellow", high = "red")
 
 Tuva_map +
