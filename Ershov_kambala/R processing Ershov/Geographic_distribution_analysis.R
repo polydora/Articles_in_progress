7fd2b71eea42b93a40a349474b0433ef9b48d986
Vadim Khaitov <polydora@rambler.ru>
2022-09-13 18:02:35 +0300

    Много Ершова

--- Ershov_kambala/R processing Ershov/Geographic_distribution_analysis.R
+++ Ershov_kambala/R processing Ershov/Geographic_distribution_analysis.R
@@ -1,91 +1,134 @@
 
 library(sdmpredictors)
 library(leaflet)
+library(readxl)
+# library(dplyr)
 
-# options(sdmpredictors_datadir="C:\\Users\\polyd\\AppData\\Local\\Temp\\RtmpuyXZRU/sdmpredictors")
+
+
+sites <- read_excel("Data/Points_coordinates.xlsx")
+
+sites$Lat <- as.numeric(gsub(",", ".", sites$Lat))
+sites$Lon <- as.numeric(gsub(",", ".", sites$Lon))
+sites$Left_prop <- as.numeric(gsub(",", ".", sites$Left_prop))
+
+
+
+
+
+library(raster)
+
+salinity = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Salinity.Mean.asc")
+temp = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Temperature.Mean.asc")
+silicate = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Silicate.Mean.asc")
+prim_prod = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Primary.productivity.Mean.asc")
+phytoplancton = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Phytoplankton.Mean.asc")
+phosphate = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Phosphate.Mean.asc")
+ph = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.pH.BOv2_2.asc")
+nitrate = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Nitrate.Mean.asc")
+dissolved_o2 = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Dissolved.oxygen.Mean.asc")
+current_velocity = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Current.Velocity.Mean.asc.BOv2_1.asc")
+chlorophyll = raster("D:/Data_LMBE/BIO_Oracle_predictors/Present.Surface.Chlorophyll.Mean.asc")
+
+
+
+aoi <- extent(32,34,66.2,66.4)
+
+sal_crp <- crop(salinity,aoi)
+
+plot(sal_crp)
+
+
+
+
+
+
 
 
 # Скачиваем данные по средней температуре воды на срденей глубине и срденей солености на срденей глубине. 
-environment.bottom <- load_layers( layercodes = c("BO2_tempmean_bdmean" ,  "BO2_salinitymean_bdmean",  "BO2_curvelmax_bdmean", 	"BO2_dissoxmin_bdmean", "BO2_ironmean_bdmean", "BO2_phosphatemean_bdmean", "BO2_carbonphytomean_bdmean", "BO2_ppmean_bdmean") , equalarea=FALSE, rasterstack=TRUE)
-
+# environment.bottom <- load_layers( layercodes = c("BO2_tempmean_bdmean" ,  "BO2_salinitymean_bdmean",  "BO2_curvelmax_bdmean", 	"BO2_dissoxmin_bdmean", "BO2_ironmean_bdmean", "BO2_phosphatemean_bdmean", "BO2_carbonphytomean_bdmean", "BO2_ppmean_bdmean") , equalarea=FALSE, rasterstack=TRUE)
 
 
 # Explore layers in a dataset
-layers <- list_layers()
-str(layers)
+# layers <- list_layers()
+# str(layers)
+
+# layers_correlation(layers)
 
 
 
 # bathymetry <- load_layers("BO_bathymean")
 
-points <- read.csv("Data/point_proportion.csv")
+# points <- read.csv("Data/point_proportion.csv")
  
 
 
-sites <- read.csv("Data/point_coordinates.csv")
+
+# initial_sites <- data.frame(Lon1=sites$Long + sites$Long_m/60, Lat1= sites$Lat + sites$Lat_m/60)
+
+
+
+my.sites <- as.matrix(sites[, 4:3])
+
+
+my.sites
+
+
+
+sites.environment <- data.frame(Lat = sites$Lat, Lon = sites$Lon,
+                                Site = sites$Site,
+                                Left_prop = sites$Left_prop/100,
+                                Temp = extract(temp,my.sites[,1:2]),
+                                Sal = extract(salinity,my.sites[,1:2]),
+                                Prim_prod = extract(prim_prod,my.sites[,1:2]),
+                                Chlorophyll = extract(chlorophyll,my.sites[,1:2]),
+                                Silicate = extract(silicate,my.sites[,1:2]),
+                                Phytopl = extract(phytoplancton,my.sites[,1:2]),
+                                Phosph = extract(phosphate,my.sites[,1:2]),
+                                pH = extract(ph,my.sites[,1:2]),
+                                Nitr = extract(nitrate,my.sites[,1:2]),
+                                Dissolv_O2 = extract(dissolved_o2,my.sites[,1:2]),
+                                Current_V = extract(current_velocity,my.sites[,1:2])
+                                )
 
 
 
 
-my.sites <- data.frame(Lon=sites$Long + sites$Long_m/60, Lat= sites$Lat + sites$Lat_m/60)
-
-my.sites[2,] <- c(37.898529, 64.020950)
-
-my.sites[8,] <- c(5.404947, 53.530350) 
-
-my.sites[11,] <- c(-4.428864, 52.229355) 
-
-my.sites[13,] <- c(22.639058, 58.182786)
-
-my.sites[17,] <- c(-4.257812, 50.334937) 
-
-my.sites[18,] <- c(8.327916, 54.593484) 
-
-my.sites[22,] <- c(-4.182711, 50.305612)
-
-my.sites[23,] <- c(1.007030, 51.605326)
-
-my.sites[26,] <- c(10.836218, 58.905289)
-
-my.sites[35,] <- c(40.098553, 64.879995)
 
 
-library(raster)
-my.sites.environment <- data.frame(Lat = my.sites$Lat, Lon = my.sites$Lon, extract(environment.bottom,my.sites[,1:2]) )
+wsbs <- c("S14", "S21", "S16", "S02", "S01", "S32", "S33", "S03", "S34", "S05", "S35", "S24", "S04")
 
+wsbs_sites.env <- sites.environment %>% filter(Site %in% wsbs)
 
-names(my.sites.environment) <- c("Lat", "Lon", "Temp", "Sal", "Curve", "Dissox", "Iron", "Phosph", "Carb_phyto", "PrProd")
+sites.env <- sites.environment %>% filter(!(Site %in% wsbs))
 
 
 
-
-my.sites.environment$Left_prop <- points$Left_prop/100
-
-
-library(ggplot2)
-
-ggplot(my.sites.environment, aes(x = PrProd, y = Left_prop )) + geom_point()
 
 
 library(betareg)
 library(car)
 
-mod <- betareg(Left_prop ~   Temp + Sal + Curve  +  Phosph + PrProd, data = my.sites.environment)
 
-
-my.sites.environment$Fi_left <- 2*asin(sqrt(my.sites.environment$Left_prop))*180/3.14
-
-
-
-mod2 <- lm(Fi_left ~   Temp + Sal + Curve  +  Phosph + PrProd, data = my.sites.environment)
-
-
+mod <- betareg(Left_prop ~  Sal +  Lon, data = sites.environment)
 
 vif(mod)
+
+# plot(mod)
 summary(mod)
 
-plot(mod)
 
 
-plot(mod2)
-summary(mod2)
+
+
+
+
+my.sites$Name <- paste("S",1:nrow(my.sites), sep = "_")
+
+m <- leaflet()
+m <- addTiles(m)
+m <- addMarkers(m, lng=sites$Lon, lat=sites$Lat, popup=sites$Site)
+m <- addCircleMarkers(m, lng=sites$Lon, lat=sites$Lat, radius = 5)
+
+addCircleMarkers(m, lng=initial_sites$Lon1, lat=initial_sites$Lat1, radius = 5, color = "green")
+
---
+++ Systellaspis_debilis/R_calc_Systellaspis/morphological_analysis.R
@@ -0,0 +1,161 @@
+library(readxl)
+library(dplyr)
+library(vegan)
+library(ggplot2)
+
+
+sys <- read_excel("Data/Sd_morph2.xlsx", na = "NA")
+
+hapl <- read_excel("Data/haplogroups.xlsx", na = "NA")
+
+sys <- merge(sys, hapl)
+
+
+tr_excluded <- c("3_pereopod_ischium_ant_spines", "3_pereopod_caprus_ant_spines", "3_pereopod_caprus_post_spines", "4_pereopod_caprus_ant_spines", "4_pereopod_caprus_post_spines", "5_pereopod_caprus_ant_spines", "karina")
+
+sys <- sys %>% select(!tr_excluded)
+
+
+
+ggplot(sys, aes(x = length)) + geom_histogram(bins = 10) + facet_wrap(~sex, nrow = 2)
+
+ggplot(sys, aes(x = length, y = height)) + geom_point() 
+
+
+
+sys %>% filter(sex == "m") %>% pull(length) %>% min()
+
+
+nrow(sys)
+
+sys2_predictors <- sys%>% filter(length > 6.5) %>% filter(complete.cases(.)) %>%  select(c(sample, height, length, sample, coordinates_Lat, coordinates_Long, location, sex, haplotype)) 
+
+sys2 <- sys %>% filter(length > 6.5) %>% filter(complete.cases(.))  %>% select(-c(sample, height, length, coordinates_Lat, coordinates_Long, location, sex, haplotype))
+  
+
+
+
+
+
+
+nrow(sys2)
+
+mod_cca <- cca(sys2 ~ length  + height, data = sys2_predictors)
+
+plot(mod_cca, display = c("species", "cn"))
+
+
+summary(mod_cca)
+
+
+
+cca_unconstr <- scores(mod_cca, choices = c("CA1", "CA2"))$sites %>% as.data.frame()
+
+ca_unconstr_pred <- cbind(sys2_predictors, cca_unconstr )
+
+
+
+
+
+ggplot(ca_unconstr_pred, aes(x = CA1, y = CA2, color = location) ) + geom_point() + geom_vline(xintercept = 0) 
+
+#Все разваливается на две группы?
+
+
+
+
+ggplot(ca_unconstr_pred, aes(x = location, y = CA1)) + geom_boxplot() 
+
+
+
+
+ggplot(ca_unconstr_pred, aes(x = CA2)) + geom_histogram(bins = 10) 
+
+
+
+ggplot(ca_unconstr_pred, aes(x = coordinates_Long, y = CA2, color = haplotype)) + geom_point() 
+
+
+
+
+ggplot(ca_unconstr_pred, aes(x = haplotype, y = CA2)) + geom_boxplot() 
+
+ggplot(ca_unconstr_pred, aes(x = haplotype, y = CA1)) + geom_boxplot() 
+
+ggplot(ca_unconstr_pred, aes(x = CA2)) + geom_histogram(bins = 10) + facet_wrap(~haplotype, nrow = 2) 
+
+
+
+
+
+qplot(x = ca_unconstr_pred$length, y = ca_unconstr_pred$CA1) + geom_smooth(method = "lm")
+
+qplot(x = ca_unconstr_pred$length, y = ca_unconstr_pred$CA2) + geom_smooth(method = "lm")
+
+
+
+
+
+ggplot(ca_unconstr_pred, aes(x = location, y = CA1)) + geom_boxplot()
+
+ggplot(ca_unconstr_pred, aes(x = location, y = CA2)) + geom_boxplot()
+
+
+
+
+lm_sys <- lm(CA1 ~ location + haplotype + sex, data = ca_unconstr_pred)
+summary(lm_sys)
+
+lm_sys <- lm(CA2 ~ location + haplotype + sex, data = ca_unconstr_pred)
+summary(lm_sys)
+
+
+
+lm_sys <- lm(CA1 ~  + sex, data = ca_unconstr_pred)
+summary(lm_sys)
+
+lm_sys <- lm(CA2 ~ location + sex, data = ca_unconstr_pred)
+summary(lm_sys)
+
+
+
+
+
+
+
+
+cca_CA1 <- scores(mod_cca, choices = c("CA1"))$species %>% as.data.frame()
+
+
+data.frame(location = sys2_predictors$location, tr = sys2$'5_pereopod_ischium_ant_spines') %>% group_by(location) %>% summarise(mean = mean(tr))
+
+
+data.frame(location = sys2_predictors$location, tr = sys2$'4_pereopod_ischium_ant_spines') %>% group_by(location) %>% summarise(mean = mean(tr))
+
+
+data.frame(location = sys2_predictors$location, tr = sys2$'v_teeth') %>% group_by(location) %>% summarise(mean = mean(tr))
+
+
+
+
+
+cca_CA2 <- scores(mod_cca, choices = c("CA2"))$species %>% as.data.frame()
+
+
+
+
+
+
+
+data.frame(location = sys2_predictors$location, tr = sys2$'5_pereopod_ischium_ant_spines') %>% group_by(location) %>% summarise(mean = mean(tr))
+
+
+data.frame(location = sys2_predictors$location, tr = sys2$'4_pereopod_ischium_ant_spines') %>% group_by(location) %>% summarise(mean = mean(tr))
+
+
+data.frame(location = sys2_predictors$location, tr = sys2$'v_teeth') %>% group_by(location) %>% summarise(mean = mean(tr))
+
+
+
+
+
