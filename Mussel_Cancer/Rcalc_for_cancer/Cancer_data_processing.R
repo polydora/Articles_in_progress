4d0192ba9d8d40681e5624fccf08edefb75f282b
Vadim Khaitov <polydora@rambler.ru>
2022-10-22 13:17:39 +0300

    пишу статью про хищников и работаю с практикой

--- Mussel_Cancer/Rcalc_for_cancer/Cancer_data_processing.R
+++ Mussel_Cancer/Rcalc_for_cancer/Cancer_data_processing.R
@@ -13,31 +13,59 @@ myt$Dist2 <- as.numeric(myt$Dist2)
 myt$Lon <- as.numeric(myt$Lon)
 myt$Lat <- as.numeric(myt$Lat)
 
-
-
-
-
 myt <- myt %>% mutate(Prop_ill = N_ill/Sample_size)
 
 
-myt$Prop_ill[myt$Prop_ill == 0] <-0.0001 
+siz <- read_excel("Data/Ecological data on mussels.xlsx", na = "NA", sheet = "Magadan_Mussels")
+siz$L <- as.numeric(siz$L)
+
+
+hist(siz$L)
+
+demogr <- siz %>% group_by(Site) %>% summarise(Prop_adult = mean(L>15, na.rm = T), N_dots_med = median(N_dots, na.rm = T), N_dots_mean = mean(N_dots, na.rm = T), L_mean = median(L, na.rm = T))
+
+
+  plot(demogr$N_dots_mean, demogr$N_dots_med)
+
+
+
+myt2 <- merge(myt, demogr)
+
+myt2$Prop_ill[myt$Prop_ill == 0] <-0.00001 
 
 
 # myt$Current <- extract(current_velocity, my.sites[, 2:1])
 
-Mod <- betareg(Prop_ill ~   N + S   + Fetch , data = myt )
+
+Mod <- betareg(Prop_ill ~   N  + S + Fetch + N_dots_mean, data = myt2 )
+
 
 
 vif(Mod)
 
-plot(myt$Dist, myt$Fetch)
+plot(Mod, which = 2)
 
+
+qplot(x = fitted(Mod), y = residuals(Mod, type = "pearson")) + geom_smooth() + geom_hline(yintercept = 0)
 
 summary(Mod)
 
 
 
-ggplot(myt, aes(x = Fetch ,  y = Prop_ill)) + geom_point()
+library(ggplot2)
+
+ggplot(myt2, aes(x = Fetch ,  y = Prop_ill)) + geom_point()
+
+ggplot(myt2, aes(y = N_dots_mean ,  x = Prop_ill)) + geom_point()
+
+siz$Site <- factor(siz$Site, levels = myt2 %>% arrange(Prop_ill) %>% pull(Site))
+  
+ggplot(siz, aes(x = Site, y = N_dots)) + geom_boxplot()
+
+
+
+
+
 
 
 
