40236011a643446f743eddec6e4d0d2debbca3c8
Vadim Khaitov <polydora@rambler.ru>
2022-11-04 11:48:18 +0300

    сделал скрипт для Иры

--- Mussel_Cancer/Rcalc_for_cancer/Cancer_data_processing.R
+++ Mussel_Cancer/Rcalc_for_cancer/Cancer_data_processing.R
@@ -2,7 +2,9 @@ library(readxl)
 library(dplyr)
 library(betareg)
 library(car)
+library(ggplot2)
 
+#################### Групповой анализ #################
 
 myt <- read_excel("Data/Magadan_data.xlsx")
 
@@ -37,7 +39,7 @@ myt2$Prop_ill[myt$Prop_ill == 0] <-0.00001
 # myt$Current <- extract(current_velocity, my.sites[, 2:1])
 
 
-Mod <- betareg(Prop_ill ~   N  + S + Fetch + N_dots_mean, data = myt2 )
+Mod <- betareg(Prop_ill ~   scale(N) +  scale(N_dots_mean)  + scale(S) + scale(Fetch), data = myt2 )
 
 
 
@@ -48,11 +50,10 @@ plot(Mod, which = 2)
 
 qplot(x = fitted(Mod), y = residuals(Mod, type = "pearson")) + geom_smooth() + geom_hline(yintercept = 0)
 
-summary(Mod)
+
+summary(Mod) #Параметры модели
 
 
-
-library(ggplot2)
 
 ggplot(myt2, aes(x = Fetch ,  y = Prop_ill)) + geom_point()
 
@@ -63,45 +64,7 @@ siz$Site <- factor(siz$Site, levels = myt2 %>% arrange(Prop_ill) %>% pull(Site))
 ggplot(siz, aes(x = Site, y = N_dots)) + geom_boxplot()
 
 
-
-
-
-
-
-
-
-
-
-myt_individ <- read_excel("Data/mussel_individual.xlsx")
-
-myt_individ$BTN_presence <- as.numeric(myt_individ$BTN_presence)
-
-
-myt_individ <- merge(myt_individ, myt, by = "Site", all.x = T )
-
-
-library(lme4)
-
-Mod_ind <- glm(BTN_presence ~  scale(L) + scale(DL)  + scale(S) + scale(Fetch) + Sex_ident, data = myt_individ, family = "binomial")
-
-vif(Mod_ind)
-
-
-summary(Mod_ind)
-
-
-
-
-
-Mod_ind <- lm( DL ~  L + S + Fetch + Sex_ident + factor(BTN_presence) , data = myt_individ)
-
-vif(Mod_ind)
-
-
-summary(Mod_ind)
-
-
-
+# Визуализация модели
 library(mgcv)
 
 mod2 <- gam(Prop_ill ~ Fetch, data = myt, family = "betar")
@@ -120,4 +83,32 @@ ggplot(MyData, aes(x = Fetch, y = Fitted)) +
   geom_ribbon(aes(ymin = Fitted - 1.96*SE, ymax = Fitted + 1.96*SE), alpha = 0.2) + 
   geom_point(data = myt, aes(y = Prop_ill), size = 2) + 
   theme_bw()
+
+
+
+
+
+
+
+
+########################### Индивидуальный анализ мидий ###################
+
+myt_individ <- read_excel("Data/mussel_individual.xlsx")
+
+myt_individ$BTN_presence <- as.numeric(myt_individ$BTN_presence)
+
+
+myt_individ <- merge(myt_individ, myt, by = "Site", all.x = T )
+
+
+library(lme4)
+
+Mod_ind <- glm(BTN_presence ~  scale(L) + scale(DL) + scale(S) + scale(Fetch) + Sex_ident, data = myt_individ, family = "binomial")
+
+vif(Mod_ind)
+
+
+summary(Mod_ind)
+
+
 
