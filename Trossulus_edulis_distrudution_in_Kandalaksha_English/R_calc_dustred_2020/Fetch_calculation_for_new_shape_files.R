2719f8f2cabfdd5e3f42c44c61408fcb2a7858d1
Vadim Khaitov <polydora@rambler.ru>
2024-02-06 10:36:34 +0300

    Переделал шейп-файлы и фетч анализ

--- Trossulus_edulis_distrudution_in_Kandalaksha_English/R_calc_dustred_2020/Fetch_calculation_for_new_shape_files.R
+++ Trossulus_edulis_distrudution_in_Kandalaksha_English/R_calc_dustred_2020/Fetch_calculation_for_new_shape_files.R
@@ -33,65 +33,160 @@ library(readxl)
 
 # Задаем пределы координат для карт
 
-Tuva_x <- c(33.4,  33.65)
+# Tuva_x <- c(33.4,  33.65)
+# 
+# Tuva_y <- c(69.17, 69.23)
+# 
+# 
+# Tuva_x_small <- c(33.56,  33.65)
+# 
+# Tuva_y_small <- c(69.17, 69.21)
 
-Tuva_y <- c(69.17, 69.23)
 
 
-Tuva_x_small <- c(33.56,  33.65)
+Kand_x <- c(32, 36.2)
+Kand_y <- c(65.6, 67.25)
 
-Tuva_y_small <- c(69.17, 69.21)
 
 
 # read shape file into R
-murm_shape <- readShapeSpatial("Maps/boundary-polygon-land-lvl4.shp", proj4string = CRS("+proj=longlat +datum=WGS84"))
+murm_shape <- readShapeSpatial("Maps/Murmanskaya_obl/boundary-polygon-land-lvl4.shp", proj4string = CRS("+proj=longlat +datum=WGS84"))
+
+karel_shape <- readShapeSpatial("Maps/Karelia/boundary-polygon-land-lvl4.shp", proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 
-gg_murm <- fortify(murm_shape)
+# Объединение шейп-файлов
+library(raster)
+
+Kand_shape <- bind(murm_shape, karel_shape)
+
+plot(Kand_shape)
 
 
-Tuva_map <- 
-ggplot(gg_murm, aes(x = long, y = lat, group = group)) + 
+
+
+# Карта для ggplot
+gg_murm_karel <- fortify(Kand_shape)
+
+
+# Tuva_map <- 
+# ggplot(gg_murm, aes(x = long, y = lat, group = group)) + 
+#   geom_polygon(fill = "gray20") +
+#   # coord_map(xlim = c(150., 151.52), ylim = c(59.45, 59.8) )+
+#   coord_map(xlim = Tuva_x_small, ylim = Tuva_y_small) +
+#   theme_bw()
+
+Kand_map <- 
+  ggplot(gg_murm_karel, aes(x = long, y = lat, group = group)) + 
   geom_polygon(fill = "gray20") +
   # coord_map(xlim = c(150., 151.52), ylim = c(59.45, 59.8) )+
-  coord_map(xlim = Tuva_x_small, ylim = Tuva_y_small) +
+  coord_map(xlim = Kand_x, ylim = Kand_y) +
   theme_bw()
 
-
-
+Kand_map
 
 
 ## Данные по поселениям мидий
 
-points <- read_excel("data/TuMyt_2009_2010_for_SDM.xlsx")
+# points <- read_excel("data/TuMyt_2009_2010_for_SDM.xlsx")
+
+myt_full <- read_excel("data/myt_full.xls")
+
+
+
+# detach("package:raster", unload = TRUE)
+# detach("package:dplyr", unload = TRUE)
+# 
+# library(dplyr)
+
+myt_site <- myt_full %>% 
+  group_by(Site) %>% 
+  select(Lat, Lon, N_T, N_E, Salinity, Min_dist_river, River, River_Size, Min_dist_river_Large, Min_dist_port, Port, Port_Status, Average_Fetch, Dist_cut) %>% 
+  summarise(Lat = mean(Lat), Lon = mean(Lon), N_T = sum(N_T), N_E = sum(N_E), Salinity = mean(Salinity), Min_dist_river = mean(Min_dist_river), River = unique(River), River_Size = unique(River_Size), Min_dist_river_Large = mean(Min_dist_river_Large),  Min_dist_port = mean(Min_dist_port), Port = unique(Port), Port_Status = unique(Port_Status), Average_Fetch = mean(Average_Fetch),   Dist_cut = mean(Dist_cut)) %>% 
+  mutate(Prop_T = N_T/(N_T+N_E))
+
+
+points <- myt_site %>% select(Site, Lat, Lon) 
+
+
 points$long <- as.numeric(points$Lon)
 points$lat <- as.numeric(points$Lat)
 
- points$long_corrected <- as.numeric(points$Lon_corrected)
- points$lat_corrected <- as.numeric(points$Lat_corrected)
+ points$long_corrected <- points$long
+ points$lat_corrected <- points$lat
 
 # Создем датафрейм с координатами точек 
 fetch.df = data.frame(
   lon = points$long_corrected, 
   lat = points$lat_corrected,
-  Site = points$Sample_ID)
+  Site = points$Site)
 
 
 fetch_locs = SpatialPoints(fetch.df[, 1:2], CRS(proj4string(murm_shape)))
 
+# plot(fetch_locs)
+
+library(waver)
+
+fetch <- fetch_len_multi(pts = fetch_locs, bearings = c(0, 45, 90, 135, 180, 225, 270, 215), shoreline = Kand_shape,  dmax = 100000, spread = 0,  method = "btree", projected = FALSE)
 
 
-fetch <- fetch_len_multi(pts = fetch_locs, bearings = c(0, 45, 90, 135, 180, 225, 270 ), shoreline = murm_shape,  dmax = 100000, spread = 0,  method = "btree", projected = FALSE)
-
-# cbind(points, fetch ) %>% select(Sample_ID, Site, Lat, Lon, "0" )
-
-write.table(data.frame(AverageFetch = apply(fetch, MARGIN = 1, FUN =  mean)/1000), "clipboard", sep = "\t" , dec = ",")
+fetch <- 
+  cbind(points, fetch ) 
 
 
 
-fetch.df$fetch <- as.data.frame(fetch) %>% rowMeans()/1000 
 
-fetch.df$fetch <- round(fetch.df$fetch, 1)
+################## Поиск аномальных точек для Fetch-анализа по новому шейп-файлу ##############
+
+anomal_df <- 
+  fetch %>%
+  filter(!complete.cases(.))
+
+Kand_map +
+  geom_point(data = anomal_df, aes(group = 1), shape = 21, color = "blue", fill = "yellow", size = 3)
+
+anomal_x <- c(32.49, 32.51)
+anomal_y <- c(66.89, 66.9)
+
+
+anomal_sampl <- data.frame(long = 32.50190, lat = 66.896)
+
+ggplot(gg_murm_karel, aes(x = long, y = lat, group = group)) + 
+  geom_polygon(fill = "gray20") +
+  coord_map(xlim = anomal_x, ylim = anomal_y) +
+  theme_bw() +
+  geom_point(data = anomal_sampl, aes(x = long, y = lat, group = 1), shape = 21, color = "blue", fill = "yellow")
+
+#######
+
+
+
+
+
+
+fetch <-
+fetch %>% 
+  mutate(Fetch =  rowMeans(select(., 8:15)))
+  
+fetch$Fetch <- fetch$Fetch/1000
+
+df_fetch <- 
+myt_site %>% 
+  select(Site, Average_Fetch) %>% 
+  merge(., fetch) %>% 
+  select(Site, Average_Fetch, Lat, Lon, Fetch)
+
+ggplot(df_fetch_murmansk, aes(Average_Fetch, Fetch)) +
+  geom_point() + geom_abline()
+
+merge(myt_full, df_fetch)
+
+
+
+write.table(merge(myt_full, df_fetch), "data/myt_full_2024.csv", sep = ";" , dec = ",", row.names = FALSE)
+
+
 
 #########################
 
