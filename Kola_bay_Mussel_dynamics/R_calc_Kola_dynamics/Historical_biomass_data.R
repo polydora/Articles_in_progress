c240ec9aa2939fe9cbf64a9b2882ee1b7548af9a
Vadim Khaitov <polydora@rambler.ru>
2024-03-16 08:16:47 +0300

    ЛЕтопись

--- Kola_bay_Mussel_dynamics/R_calc_Kola_dynamics/Historical_biomass_data.R
+++ Kola_bay_Mussel_dynamics/R_calc_Kola_dynamics/Historical_biomass_data.R
@@ -45,6 +45,10 @@ Murm_y <- c(min(myt$Latitude)-0.1, max(myt$Latitude)+0.1)
 
 gg_murm <- fortify(Murm_shape)
 
+gg_murm <- 
+  gg_murm 
+
+
 
 Murm_map <- 
   ggplot(gg_murm, aes(x = long, y = lat, group = group)) + 
@@ -58,11 +62,17 @@ Murm_map <-
 # Разделяем датсеты по сборщикам ##############
 
 myt_Roman <- myt %>% filter(source == "Romanova")
+unique(myt_Roman$Year)
 
-myt_Mil_Ant <- myt %>% filter(source %in% c("Antipova et al", "Milyutin and Sokolov") )
+myt_Mil <- myt %>% filter(source %in% c("Milyutin and Sokolov") )
+unique(myt_Mil$Year)
 
-myt_other <-  myt %>% filter(source != "Romanova")
+myt_Ant <- myt %>% filter(source %in% c("Antipova et al") )
+unique(myt_Ant$Year)
 
+
+
+myt_other <-  myt %>% filter(!source %in% c("Antipova et al", "Milyutin and Sokolov", "Romanova")) 
 
 
 
@@ -84,19 +94,19 @@ meteo %>%
 
 ## Долготная зависимость температур в разные периоды ######
 
-meteo %>% 
-  filter(Year %in% unique(myt_Roman$Year)) %>% 
-  ggplot(aes(x = Longitude, y = Tw)) +
-  geom_point() + 
-  geom_smooth()
-  
-  
+Murm_map + geom_point(data = myt_Roman, aes(x = Longitude, y = Latitude, group = 1), color = "yellow", size = 2)  
+
+Murm_map + geom_point(data = myt_Mil_Ant, aes(x = Longitude, y = Latitude, group = 1), color = "yellow", size = 2) 
+
 
 meteo %>% 
-  filter(Year %in% unique(myt_Mil_Ant$Year)) %>% 
+  filter(Year %in% unique(myt_other$Year)) %>% 
   ggplot(aes(x = Longitude, y = Tw)) +
   geom_point() + 
   geom_smooth()
+
+Murm_map + geom_point(data = myt_other, aes(x = Longitude, y = Latitude, group = 1), color = "yellow", size = 2) 
+
 
 
 
@@ -108,7 +118,11 @@ meteo %>%
 library(mgcv)
 library(gratia)
 
-Mod_Tw <- gam(Tw ~ s(Year, bs = "cr", k = 10) + s(Longitude, Latitude, bs = "tp", k = 12), data = meteo)
+meteo_reduced <-
+  meteo %>% 
+  filter(!Platform %in% c("Port_Vladinir", "Drozdovka"))
+
+Mod_Tw <- gam(Tw ~ s(Year, bs = "cr", k = 4) + ti(Longitude, Latitude, bs = "tp"), data = meteo_reduced)
 
 summary(Mod_Tw)
 
@@ -116,23 +130,80 @@ draw(Mod_Tw)
 
 # Строим модель пространственной изменчивости температуры воздуха по данным метеостанций 
 
-Mod_Ta <- gam(Ta ~ s(Year, bs = "cr", k = 10) + s(Longitude, Latitude, bs = "tp", k = 12), data = meteo)
+Mod_Ta <- gam(Ta ~ s(Year, bs = "cr", k = 4) + ti(Longitude, Latitude, bs = "tp", k = 5), data = meteo_reduced)
 
 summary(Mod_Ta)
 
 draw(Mod_Ta)
 
 
+meteo_means <-
+  meteo_reduced %>%
+  filter(Year %in% c(1971, 1981)) %>% 
+  group_by(Year, Platform) %>% 
+  summarise(Longitude = mean(Longitude), Latitude = mean(Latitude), Tw = mean(Tw))
+  
+  
+
+meteo_means$Tw_predicted <-  predict(Mod_Tw, newdata = meteo_means)
+
+ggplot(meteo_means, aes(x = Tw_predicted, y = Tw)) +
+  geom_point() +
+  geom_abline() +
+  facet_wrap(~Platform)
 
 
-myt_Roman <- 
-myt_Roman %>% 
-  mutate(Year = year)
 
 myt_Roman <- 
   myt_Roman %>% 
   mutate(Tw = predict(Mod_Tw, newdata = myt_Roman),
          Ta = predict(Mod_Ta, newdata = myt_Roman))
+
+
+myt_Mil <- 
+  myt_Mil %>% 
+  mutate(Tw = predict(Mod_Tw, newdata = myt_Mil),
+         Ta = predict(Mod_Ta, newdata = myt_Mil))
+
+myt_Ant <- 
+  myt_Ant %>% 
+  mutate(Tw = predict(Mod_Tw, newdata = myt_Ant),
+         Ta = predict(Mod_Ta, newdata = myt_Ant))
+
+
+myt_other <- 
+  myt_other %>% 
+  mutate(Tw = predict(Mod_Tw, newdata = myt_other),
+         Ta = predict(Mod_Ta, newdata = myt_other))
+
+
+# Долготный градиент температур на разных данных
+
+ggplot(myt_Roman, aes(x = Longitude, y = Tw)) + 
+  geom_point() +
+  geom_smooth(method = "lm", color = "black") +
+  geom_point(data = myt_Mil, color = "red") + 
+  geom_smooth(data = myt_Mil, color = "red", method = "lm")  +
+  geom_point(data = myt_Ant, color = "blue") + 
+  geom_smooth(data = myt_Ant, color = "blue", method = "lm") 
+
+
+myt_Ant %>%  
+  group_by(Year) %>% 
+  summarise(Mean_Tw = mean(Tw))
+
+
+
+
+
+library(ggrepel)
+
+ggplot(data = myt_Ant) +
+  geom_polygon(data = gg_murm, aes(x = long, y = lat, group = group), fill = "gray") +
+  geom_point(aes(x = Longitude, y = Latitude, group = 1), color = "yellow", size = 2) +
+  facet_wrap(~Year) +
+  coord_map(xlim = Murm_x, ylim = Murm_y) +
+  geom_text_repel(aes(x = Longitude, y = Latitude, label = site))
 
 
 
@@ -142,21 +213,10 @@ myt_Roman <-
 
 unique(myt$source)
 
-myt_other2 <- 
-  myt_other %>% 
-  filter(source %in% c("Antipova et al", "Milyutin and Sokolov"))
+myt_train <- rbind(myt_Mil, myt_Ant, myt_Roman)
 
 
-ggplot(myt_other2, aes(Ta)) + 
-  geom_density() +
-  geom_density(data = myt_Roman, color = "blue")
-  
-
-
-myt_SDM <- gam(log(B_kg) ~ s(Tw, Ta, bs = "tp"), data = myt_Roman)
-
-myt_SDM <- gam(log(B_kg) ~ s(Tw, Ta, bs = "tp", k = 20), data = myt_other)
-
+myt_SDM <- gam(log(B_kg) ~ s(Tw, Ta, k = 10), data = myt_train)
 
 summary(myt_SDM)
 
@@ -167,154 +227,115 @@ draw(myt_SDM)
 
 ###########################
 
-myt_other <- 
-  myt_other %>% 
-  mutate(Year = year)
+
+myt_train$B_predicted <- predict(myt_SDM, newdata = myt_train)
+
+ggplot(myt_train, aes(x = B_predicted, y = log(B_kg))) + geom_point() +
+  geom_abline()
 
 
-myt_other <- 
-  myt_other %>%   
-  mutate(Tw = predict(Mod_Tw, newdata = myt_other),
-         Ta = predict(Mod_Ta, newdata = myt_other))
+myt_other$region2 <- ifelse(myt_other$region == "Dalnye Zelenetsy", "Eastern Murman", myt_other$region)
 
+myt_other$Period <- ifelse(myt_other$Year < 2000, "XX", "XXI")
 
 
 myt_other$B_predicted <- predict(myt_SDM, newdata = myt_other)
 
-myt_other$region2 <- ifelse(myt_other$region == "Dalnye Zelenetsy", "Eastern Murman", myt_other$region)
-
-ggplot(myt_other, aes(x = log(B_kg), y = B_predicted)) +
-  geom_point() +
+ggplot(myt_other, aes(x = B_predicted, y = log(B_kg))) + 
+  geom_point(aes(color = Period)) +
+  geom_abline() +
   facet_wrap(~region2)
 
 
-ggplot(myt_other, aes(x = log(B_kg), y = B_predicted)) +
-  geom_point() 
+##### Анализ динамики по выделам ################
+
+myt <- read_excel("data/data_history.xlsx")
+
+myt <- 
+  myt %>% 
+  rename(Year = year)
+
+hist(myt$B_kg)
+
+
+as.data.frame(table(myt$Year, myt$site)) %>% 
+  group_by(Var2) %>% 
+  summarise(N_Years = sum(Freq)) ->
+  observations
+
+site_include <- c("Kharlov Island_N", "Kharlov Island_S", "Tyuva", "Ura", "Klimkovka", "Dolgaya", "Zelenetskaya Zapadnaya", "Yarnyshnaya_lit", "Dal'niy Plyazh")
+
+myt %>% 
+  filter(site %in% site_include) ->
+  monitor
 
 
 
-myt_other %>% 
-  group_by(region2) %>% 
-  summarise(R_spearman = cor(log(B_kg), B_predicted, method = "spearman" ))
+ggplot(monitor, aes(x = Year, y = log(B_kg))) +
+  geom_point() +
+  facet_wrap(~ region)
 
 
-ggplot(myt_other, aes(x = Year, y = log(B_kg) )) +
-  geom_point(color = "blue") +
-  facet_wrap(~region2, scales = "free_y") +
-  geom_line(aes(y = B_predicted)) +
-  geom_point(aes(y = B_predicted), fill = "yellow", shape = 21)
+
+temp_KM <- read_excel("Data/Temperature_KM.xlsx")
 
 
-ggplot(myt_other, aes(x = Year, y = log(B_kg) )) +
-  geom_point(color = "blue") +
-  geom_smooth() +
-  geom_point(aes(y = B_predicted), fill = "yellow", shape = 21) +
-  geom_smooth(aes(y = B_predicted), color = "red")
+lag_temp <- function(Y, lag = 0){
+  require(dplyr)
+  df <- 
+    temp_KM %>% 
+    filter(Year <= Y & Year >= (Y - lag))
+  mean(df$Temp)
+}
 
 
-ggplot(myt_other %>% filter(source == "Antipova et al"), aes(x = log(B_kg), y = B_predicted)) +
-  geom_point(color = "blue") +
+lag_temp_2 <- function(Y, lag = 0){
+  require(dplyr)
+  df <- 
+    temp_KM %>% 
+    filter(Year == (Y - lag))
+  df$Temp
+}
+
+
+
+monitor$Temp_lagged <- NA
+for(i in 1:nrow(monitor)){
+  monitor$Temp_lagged[i] <- lag_temp(monitor$Year[i], lag = 5)
+}
+
+
+monitor <-
+  monitor %>% 
+  mutate(Period = ifelse(Year < 2000, "XX", "XXI"))
+
+ggplot(monitor, aes(x = Temp_lagged, y = log(B_kg), color = Period)) +
+  geom_point(size = 4) +
   geom_smooth(method = "lm")
 
-unique(myt_other$source)
+monitor$site <- factor(monitor$site)
+monitor$Period <- factor(monitor$Period)
 
 
-ggplot(myt_other %>% filter(source == "Milyutin and Sokolov"), aes(x = log(B_kg), y = B_predicted)) + geom_point(color = "blue") +
-  geom_smooth(method = "lm")
+Mod_lag_temp <- gam(log(B_kg) ~ s(Temp_lagged, by = Period, bs = "cr", k = 3) + Period + s(site, bs = "re"), method = "REML", data = monitor, family = "gaussian")
+
+summary(Mod_lag_temp)
+
+appraise(Mod_lag_temp)
+
+draw(Mod_lag_temp)
 
 
+Mydata <- monitor %>% select(Temp_lagged, Period, site, Year) %>% unique() 
 
+predicted <- predict(Mod_lag_temp, newdata = Mydata, se.fit = TRUE, exclude = "s(site)", newdata.guaranteed=TRUE)
 
-### Строим предсказания на основе SDM для Ярнышной
+Mydata$Log_B_predicted <- predicted$fit
+Mydata$SE <- predicted$se.fit
 
-###### Данные по климатическим параметрам
-clim <- read_excel("Data/climate_Murman.xlsx", sheet = "data_transposed", na = "NA")
-
-# переводим в "длинный! формат 
-clim_long <- 
-  melt(clim, id.vars = c("Param",	"Param_Type",	"Month"), variable.name = "Year") %>% 
-  filter(complete.cases(.)) %>% 
-  mutate(Year = as.numeric(as.character(Year)))
-
-clim_long %>% 
-  filter(Param_Type %in% c("Ta_mean", "Tw_mean")) %>% 
-  filter(Month != 12) %>% 
-  group_by(Year, Param_Type) %>% 
-  summarise(value = mean(value)) %>% 
-  dcast(Year ~ Param_Type) %>% 
-  rename(Ta = Ta_mean, Tw = Tw_mean) -> 
-  temp_Teriberka 
-
-
-##### Данные по биомассе в Ярнышной
-
-YaB <- read_excel("Data/Ya_lit_initial_data_B.xlsx")
-
-YaB %>% 
-  select(Year, Biomass) %>% 
-  group_by(Year) %>% 
-  summarise(Biomass=mean(Biomass))->
-  Biomass
-
-temp_Teriberka <-
-  temp_Teriberka %>% 
-  filter(Year %in% YaB$Year)
-
-Biomass$Log_Biomass <- log(Biomass$Biomass/1000)  
-
-Biomass$Biomass_predicted <- predict(myt_SDM, newdata = temp_Teriberka)
-
-qplot(Biomass$Log_Biomass, Biomass$Biomass_predicted)
-
-
-ggplot(Biomass, aes(x = Year))
-
-Biomass %>% 
-  melt(id.vars = "Year") %>% 
-  filter(variable != "Biomass") %>% 
-  ggplot(aes(x = Year, y = value, color = variable)) +
-  geom_line() 
-
-
-
-###################
-
-
-
-
-
-# Частотное Распределение значений биомасс
-
-low_biomass <- quantile(log(myt_Roman$B_kg), probs = 0.10)
-
-Pl_roman <- 
-  myt %>%
-  filter(source == "Romanova") %>%
-  ggplot(aes(log(B_kg) )) +
-  geom_histogram() +
-  xlim(-11, 6) +
-  geom_vline(xintercept = low_biomass, color = "blue")
-
-Pl_other <- 
-  myt %>%
-  filter(source != "Romanova") %>%
-  ggplot(aes(log(B_kg) )) +
-  geom_histogram() +
-  xlim(-11, 6)+
-  geom_vline(xintercept = low_biomass, color = "blue")
-
-library(cowplot)
-
-plot_grid(Pl_roman, Pl_other, ncol = 1)
-
-######################### Бинаризованные данные по биомассе
-
-myt_Roman <- 
-  myt_Roman %>% 
-  mutate(Abundance_class = ifelse(log(B_kg) <= low_biomass, "Low", "High"))
-
-
-Murm_map +
-  geom_point(data = myt_Roman, aes(x = Longitude, y = Latitude, group = 1, fill = Abundance_class), shape = 21, size = 3) +
-  scale_fill_manual(values = c("red", "yellow"))
-
+ggplot(Mydata, aes(x = Temp_lagged, y = Log_B_predicted, color = Period )) + 
+  geom_line() +
+  geom_ribbon(aes(ymin = Log_B_predicted - 1.96*SE, ymax = Log_B_predicted + 1.96*SE), alpha = 0.2) +
+  geom_point(data = monitor, aes(y = log(B_kg))) +
+  theme_bw() +
+  scale_color_manual(values = c("red", "blue"))
