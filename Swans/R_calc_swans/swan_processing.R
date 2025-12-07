51f3af4a5bffda49c11e769cde699dde948d7952
Vadim Khaitov <polydora@rambler.ru>
2025-12-07 22:10:36 +0300

    Swans

--- Swans/R_calc_swans/swan_processing.R
+++ Swans/R_calc_swans/swan_processing.R
@@ -19,7 +19,7 @@ dat2$Site <- factor(dat2$Site)
 library(mgcv)
 
 Mod <- gam(N ~ s(Year, by = Species, k = 5) +
-             s(Dist, Depth,  by = interaction(Species, Ice), k = -1) +
+             s(Dist, Depth,  by = interaction(Species, Ice), k = 5) +
              Species + Ice +
              s(Site, bs = "re", k = length(unique(dat2$Site))),
            data = dat2,
@@ -27,93 +27,34 @@ Mod <- gam(N ~ s(Year, by = Species, k = 5) +
            method = "REML")
 
 
+library(gratia)
 
 appraise(Mod)
+
 library(DHARMa)
 simulateResiduals(Mod, plot = T)
-summary(Mod)
-library(gratia)
-draw(Mod)
-draw(Mod, select = 5:9)
-draw(Mod, select = 4:9)
-sm <- smooth_estimates(Mod)  %>%
-add_confint()
-sm <-
-sm %>%
-mutate(.estimate_N = exp(.estimate + coef(Mod)["(Intercept)"]),
-.lower_N = exp(.lower_ci + coef(Mod)["(Intercept)"]),
-.upper_N = exp(.upper_ci +coef(Mod)["(Intercept)"]))
-sm
-names(sm)
-unique(sm$.smooth)
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice")
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice") %>%
-ggplot(aes(x = Dist, y = Depth, fill = .estimate_N)) %>%
-geom_tile()
-library(ggplot2)
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice") %>%
-ggplot(aes(x = Dist, y = Depth, fill = .estimate_N)) %>%
-geom_tile()
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice") %>%
-ggplot(aes(x = Dist, y = Depth, fill = .estimate_N)) +
-geom_tile()
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice") %>%
-ggplot(aes(x = Dist, y = Depth, fill = .estimate_N)) +
-geom_point()
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice") %>%
-ggplot(aes(x = Dist, y = Depth, color = .estimate_N)) +
-geom_point()
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice") %>%
-ggplot(aes(x = Dist, y = Depth, fill = .estimate_N)) +
-geom_tile() +
-scale_fill_gradient(low = "yellow", y = "red")
-sm %>%
-filter(.smooth == "s(Dist,Depth):interaction(Species, Ice)Sp1.No_ice") %>%
-ggplot(aes(x = Dist, y = Depth, fill = .estimate_N)) +
-geom_tile() +
-scale_fill_gradient(low = "yellow", high = "red")
-draw(Mod, select = 4:9)
-draw(Mod, select = 7)
-draw(Mod, select = 7) +
-guides(fill = "none")
-draw(Mod, select = 7) +
-guides(fill = "none") +
-scale_fill_gradient(low = "yellow", high = "red")
-draw(Mod, select = 7) +
-guides(fill = "none") +
-scale_fill_gradient(low = "yellow", high = "red") +
-ggtitle("Sp1 в безледный период")
-draw(Mod, select = 7) +
-guides(fill = "none") +
-scale_fill_gradient(low = "yellow", high = "red") +
-ggtitle("Sp1 в безледный период", subtitle = "")
+
+
+
+
 draw(Mod, select = 7) +
 guides(fill = "none") +
 scale_fill_gradient(low = "yellow", high = "red") +
 ggtitle("Sp1 в безледный период", subtitle = NULL)
+
+
 draw(Mod, select = 8) +
 guides(fill = "none") +
 scale_fill_gradient(low = "yellow", high = "red") +
 ggtitle("Sp2 в безледный период", subtitle = NULL)
+
+
 draw(Mod, select = 9) +
 guides(fill = "none") +
 scale_fill_gradient(low = "yellow", high = "red") +
 ggtitle("Sp3 в безледный период", subtitle = NULL)
-library(knitr)
-opts_chunk$set(echo = TRUE)
-opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 
 
 
 
 
-Mod_no_ice <- gam(N ~ s(Year, by = Species, k = 5) + s(Depth, Dist, by = Species, k = 5) + Species, data = dat2 %>% filter(Ice == 2), family = "nb")
-
-draw(Mod_no_ice, select = 4:6)
